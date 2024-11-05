a9661dd4d88c09cf4cee8c7ce1ab5e880f12984e
kevinhes <kevinhes1115@gmail.com>
2024-11-05 10:51:49 +0800

    新贈  index 並在 app.vue 新增 routerpage

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+  const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <h1>Page:首頁</h1>
+    <p>目前路由的路徑 - route.full 為 {{ route.fullPath }}</p>
+  </div>
+</template>
+
+<style scoped></style>
