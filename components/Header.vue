8408059ef6924f3863e9000087f96d9aa4c764f2
kevinhes <kevinhes1115@gmail.com>
2024-11-11 11:03:31 +0800

    設定 sass 以及 bootstrap 設置 router 的 active 設定

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    Component: Header
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <nuxt-link class="navbar-brand" to='/'>Navbar</nuxt-link>
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <nuxt-link class="nav-link" aria-current="page" to="/">Home</nuxt-link>
+      </li>
+      <li class="nav-item">
+        <nuxt-link class="nav-link" to="/room">Room</nuxt-link>
+      </li>
+    </ul>
   </div>
+</nav>
 </template>
 
 <style scoped></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  css: ['assets/scss/main.scss'],
+  router: {
+    options: {
+      linkActiveClass: "active",
+      linkExactActiveClass: "active",
+    },
+  },
 })
